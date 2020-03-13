e9034991b8dc929f4cc4a12cb1641e88ebc1c816
mews <mews@mews-base.com>
2020-03-13 13:20:50 +0900

    fix

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+all: dist/main.exe dist/configs
+
+
+
 ui/main_ui.py: ui/main.ui
 	python3 -m PyQt5.uic.pyuic -x $^ -o $@
+
+dist/main.exe: main.py ui/main_ui.py  
+	pyinstaller.exe main.py --onefile 
+
+dist/configs:
+	cp -r configs dist/
--- main.py
+++ main.py
@@ -10,16 +10,27 @@ class Ui(Ui_Dialog):
 
     def buttonClick(self):
         text = self.textEdit.toPlainText()
-        print(text)
-        endpoint = os.getenv('HOOK')
 
+        if self.to_channel.isChecked():
+            mension ='<!channel> \n ' 
+        elif self.to_here.isChecked():
+            mension='<!here> \n'
+        elif self.to_none :
+            mension = ''
         data = {
-          'text' : '<!channel> \n ' + text
+          'text' : mension + text
         }
 
         payload =  json.dumps(data)
+        config_file ='configs/url_list.txt'
 
-        requests.post(endpoint, payload)
+        for line in open(config_file, 'r').readlines():
+            sp = line.split(',')
+            desc = sp[0].strip()
+            url = sp[1].strip()
+            ret = requests.post(url, payload)
+            print('send success {}'.format(desc))
+
 
 if __name__ == "__main__":
     import sys
